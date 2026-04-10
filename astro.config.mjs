2d111dd53c2f1089c963079874933445e3c2fe5e
Chris Dobbyn <8264498+cdobbyn@users.noreply.github.com>
2026-04-10 08:34:33 -0700

    chore: set outDir=./dist/valheim and rename package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import solid from '@astrojs/solid-js';
 export default defineConfig({
   site: 'https://www.dobbo.ca',
   base: '/valheim/',
+  outDir: './dist/valheim',
   trailingSlash: 'always',
   integrations: [solid()],
 });
